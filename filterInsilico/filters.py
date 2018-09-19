b5530ad747f65e0bd64b3b53bce6af64e1302819
felipe <tifonzafel@gmail.com>
2018-09-19 17:16:59 +0200

    added filtering by functional group

--- filterInsilico/filters.py
+++ filterInsilico/filters.py
@@ -3,19 +3,33 @@ from typing import (Dict, List)
 import pandas as pd
 
 
-def apply_filters(dict_input: Dict):
+def apply_filters(dict_input: Dict) -> pd.DataFrame:
     """
+    Apply a different set of filters specified in `dict_input`
+    to a molecular set.
+
+    :returns: Pandas Dataframe
     """
     input_file = dict_input['input_file']
-    smiles = read_smiles(input_file)
-    print(len(list(smiles)))
+    molecules = pd.read_table(input_file, header=None, names=["smiles"])
+
+    filters = dict_input['filters']
+    if 'functional_groups' in filters:
+        df = filter_by_functional_group(molecules, filters['functional_groups'])
+
+    return df
 
 
-def read_smiles(file_path: str) -> List:
+def filter_by_functional_group(molecules: pd.DataFrame, functional_groups: List) -> pd.DataFrame:
     """
-    Read smiles from file.
+    Search for a set of functional_groups
     """
-    with open(file_path, 'r') as f:
-        xs = f.read().split()
+    # Transform molecules to rkdit molecules
+    mols = molecules.smiles.apply(lambda x: Chem.MolFromSmiles(x))
+    patterns = [Chem.MolFromSmiles(f) for f in functional_groups['smiles']]
 
-    return map(Chem.MolFromSmiles, xs)
+    # Check if the functional_groups are in the molecules
+    molecules['has_functional_group'] = mols.apply(
+        lambda m: any(m.HasSubstructMatch(p) for p in patterns))
+
+    return molecules
